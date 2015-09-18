53c6a3d7cdcb215a2aca46d0abc90616a779feaf
Blackstock <charlieblackstock@gmail.com>
2015-09-18 10:56:07 -0600

    setup checkout view and styling

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ app.config(['$routeProvider', function($routeProvider) {
     templateUrl: "../partials/home.html",
     controller: "HomeController"
   })
+  .when('/checkout', {
+    templateUrl: "../partials/checkout.html",
+    controller: "HomeController"
+  })
   .otherwise({
     redirectTo: '/'
   });
